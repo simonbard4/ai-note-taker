ff1ad0e029f9ec7a0189f706163a1332e26ff424
Simon Bard <17sb69@queensu.ca>
2025-04-20 11:25:32 -0400

    build home page

---
+++ src/components/NoteTextInput.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+type Props = {
+  noteId: string;
+  startingNoteText: string;
+};
+
+function NoteTextInput({ noteId, startingNoteText }: Props) {
+  return <div>NoteTextInput</div>;
+}
+
+export default NoteTextInput;
